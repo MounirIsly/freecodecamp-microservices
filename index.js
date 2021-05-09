b77d3ee1ae541e39737f7ef9ec5935bfa5731c77
Mounir Isly <munnerbey@gmail.com>
2021-05-10 00:55:55 +0100

    serve static files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = new express();
 
+app.use('/public', express.static(__dirname + "/public"))
+
 app.get('/', (req, res)=> {
-    res.send('Hello')
+    res.sendFile(__dirname + "/views/index.html")
 })
 app.listen(3000, ()=>console.log('App running'))
